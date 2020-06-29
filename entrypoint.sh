c5db63f1f01caee92dbac6888c52767a872927e2
Chad Crowell <chad.crowell@linuxacademy.com>
2020-06-29 14:46:31 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 echo "hello $INPUT_MYINPUT"
 memory=$(cat /proc/meminfo) # get the mem total
 echo "::set-output name=memory::$memory"
-echo "::add-path name=path::/var/www"
+path='/var/www'
+echo "::add-path name=path::$path"

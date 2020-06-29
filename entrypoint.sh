451817815a83c75bfa3e1e2ec128b821aded19db
Chad Crowell <chad.crowell@linuxacademy.com>
2020-06-29 14:49:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "hello $INPUT_MYINPUT"
 memory=$(cat /proc/meminfo) # get the mem total
 echo "::set-output name=memory::$memory"
-path='/var/www'
-echo "::add-path name=path::$path"
+MY_NAME="Mona The Octocat"
+echo "::add-mask::$MY_NAME"
